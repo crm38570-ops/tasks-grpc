90c9b6d14a36d4e927d0f151778e1d2c9c1b275b
Danil Holikov <break.build.one.minute@gmail.com>
2026-08-05 13:23:59 +0300

    feat: configure TypeORM connection and global validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
