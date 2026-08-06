0bac1d525e927c988470b05226c9412b2a8c6483
Danil Holikov <break.build.one.minute@gmail.com>
2026-08-06 15:54:24 +0300

    feat(auth): read jwt secret from config service

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,12 +4,16 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UsersRepository } from './users.repository';
 import { JwtPayload } from './types';
 import { User } from './user.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private userRepository: UsersRepository) {
+  constructor(
+    private userRepository: UsersRepository,
+    private configService: ConfigService,
+  ) {
     super({
-      secretOrKey: 'someSecret',
+      secretOrKey: configService.get('JWT_SECRET')!,
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
