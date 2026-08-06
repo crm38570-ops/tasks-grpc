2a5e8b40b0352a96f42a0c272605ea7aef007d2b
Danil Holikov <break.build.one.minute@gmail.com>
2026-08-06 13:18:06 +0300

    feat: add startup log in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './interceptors';
 
 async function bootstrap() {
+  const logger = new Logger();
+  const PORT = process.env.PORT ?? 3000;
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
+  logger.log(`App started on PORT: ${PORT}`);
 }
 bootstrap().catch((error) => {
   console.error(error);
