dc8e8f27f513336ce19d701af4685ca413d7d8be
Даниил Голиков <crm.38570@gmail.com>
2026-08-08 09:49:49 +0300

    refactor(swagger): remove swagger decorators from entities

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,35 +1,19 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Task } from '../tasks/task.entity';
 import { Exclude } from 'class-transformer';
-import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
-  @ApiProperty({
-    example: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
-    description: 'UUID пользователя',
-    format: 'uuid',
-  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ApiProperty({
-    example: 'Marsianin',
-    description: 'Уникальное имя пользователя',
-  })
   @Column({ unique: true })
   username: string;
 
-  @ApiProperty({
-    example: 'M@k@rony44!',
-    description: 'Хэш пароля (только на запись)',
-    writeOnly: true,
-  })
   @Exclude({ toPlainOnly: true })
   @Column()
   password: string;
 
-  @ApiHideProperty()
   @OneToMany(() => Task, (task) => task.user, { eager: true })
   tasks: Task[];
 }
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -2,42 +2,21 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 import { User } from '../auth/user.entity';
 import { Exclude } from 'class-transformer';
-import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Task {
-  @ApiProperty({
-    example: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
-    description: 'UUID задачи',
-    format: 'uuid',
-  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ApiProperty({
-    example: 'Написать диссертацию',
-    description: 'Заголовок задачи',
-  })
   @Column()
   title: string;
 
-  @ApiProperty({
-    example: '"Капибары, почему все мы их так любим?"',
-    description: 'Подробное описание задачи',
-  })
   @Column()
   description: string;
 
-  @ApiProperty({
-    enum: TaskStatus,
-    enumName: 'TaskStatus',
-    example: TaskStatus.OPEN,
-    description: 'Текущий статус задачи',
-  })
   @Column()
   status: TaskStatus;
 
-  @ApiHideProperty()
   @ManyToOne(() => User, (user) => user.tasks, { eager: false })
   @Exclude({ toPlainOnly: true })
   user: User;
