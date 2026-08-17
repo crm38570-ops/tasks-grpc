4962e154fbde2eb50bb15ab0b3d8f0c0626a8348
Даниил Голиков <crm.38570@gmail.com>
2026-08-17 16:17:38 +0300

    fix(http): preserve streamed file responses

--- src/interceptors/transform.interceptor.ts
+++ src/interceptors/transform.interceptor.ts
@@ -1,4 +1,9 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  StreamableFile,
+} from '@nestjs/common';
 import { instanceToPlain } from 'class-transformer';
 import { Observable, map } from 'rxjs';
 
@@ -7,6 +12,12 @@ export class TransformInterceptor implements NestInterceptor {
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    return next.handle().pipe(map((data) => instanceToPlain(data)));
+    return next
+      .handle()
+      .pipe(
+        map((data) =>
+          data instanceof StreamableFile ? data : instanceToPlain(data),
+        ),
+      );
   }
 }
