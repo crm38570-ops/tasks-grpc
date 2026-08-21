7985b755fecfd70d8f696d4b88882c34427cf7a8
crm38570-ops <crm.38570@gmail.com>
2026-08-21 13:32:48 +0300

    feat: add global gRPC exception filter with logging
    Register GrpcExceptionFilter via APP_FILTER so RpcException is rethrown
    with original status code, and log the exception stack in the filter.

--- files-service/src/common/filters/grpc-exception.filter.ts
+++ files-service/src/common/filters/grpc-exception.filter.ts
@@ -1,18 +1,20 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { status } from '@grpc/grpc-js';
 
 @Catch()
 export class GrpcExceptionFilter implements ExceptionFilter {
+  private logger = new Logger('GrpcExceptionFilter', { timestamp: true });
+
   catch(exception: unknown, _host: ArgumentsHost) {
     if (exception instanceof RpcException) {
+      this.logger.error(exception.stack);
       throw exception;
     }
 
     void _host;
 
-    console.error(exception);
-
+    // Здесь нужно доделать логирование
     throw new RpcException({
       code: status.INTERNAL,
       message: 'Внутренняя ошибка сервера',
