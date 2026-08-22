e351ff469f92e23e7e30b060474257a6d92d7113
crm38570-ops <crm.38570@gmail.com>
2026-08-22 14:30:55 +0300

    refactor(gateway): group proxy controllers by service

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { GatewayModule } from './gateway/gateway.module';
-import { AuthProxyModule } from './proxies/auth-proxy.module';
-import { TasksProxyModule } from './proxies/tasks-proxy.module';
-import { FilesProxyModule } from './proxies/files-proxy.module';
+import { AuthProxyModule } from './proxies/auth/auth-proxy.module';
+import { TasksProxyModule } from './proxies/tasks/tasks-proxy.module';
+import { FilesProxyModule } from './proxies/files/files-proxy.module';
 
 @Module({
   imports: [GatewayModule, AuthProxyModule, TasksProxyModule, FilesProxyModule],
