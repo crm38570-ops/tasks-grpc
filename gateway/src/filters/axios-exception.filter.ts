99161ba28db7f5e7a15ae4609844d7cb40b8f557
crm38570-ops <crm.38570@gmail.com>
2026-08-26 11:09:40 +0300

    fix(gateway): stop leaking downstream response body in AxiosExceptionFilter

--- gateway/src/filters/axios-exception.filter.ts
+++ gateway/src/filters/axios-exception.filter.ts
@@ -1,10 +1,17 @@
 import {
   BadGatewayException,
+  BadRequestException,
   Catch,
+  ConflictException,
   ExceptionFilter,
+  ForbiddenException,
   GatewayTimeoutException,
   HttpException,
   InternalServerErrorException,
+  NotFoundException,
+  ServiceUnavailableException,
+  UnauthorizedException,
+  UnprocessableEntityException,
 } from '@nestjs/common';
 import { AxiosError } from 'axios';
 
@@ -12,12 +19,30 @@ import { AxiosError } from 'axios';
 export class AxiosExceptionFilter implements ExceptionFilter {
   catch(exception: AxiosError) {
     if (exception.response) {
-      const { status, data } = exception.response;
+      const { status } = exception.response;
 
-      throw new HttpException(
-        typeof data === 'string' ? data : (data as Record<string, any>),
-        status,
-      );
+      switch (status) {
+        case 400:
+          throw new BadRequestException();
+        case 401:
+          throw new UnauthorizedException();
+        case 403:
+          throw new ForbiddenException();
+        case 404:
+          throw new NotFoundException();
+        case 409:
+          throw new ConflictException();
+        case 422:
+          throw new UnprocessableEntityException();
+        case 429:
+          throw new HttpException('Too Many Requests', 429);
+        case 500:
+          throw new InternalServerErrorException();
+        case 503:
+          throw new ServiceUnavailableException();
+        default:
+          throw new HttpException('Upstream service error', status);
+      }
     } else if (exception.code) {
       const { code } = exception;
 
