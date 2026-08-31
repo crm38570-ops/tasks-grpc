d9af9d907002b7c060a4579eb85bd064925a1db7
crm38570-ops <crm.38570@gmail.com>
2026-08-31 14:31:41 +0300

    feat(gateway): switch auth proxy to gRPC client

--- gateway/src/proxies/auth/auth-proxy.module.ts
+++ gateway/src/proxies/auth/auth-proxy.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { AuthProxyController } from './auth-proxy.controller';
+import { ClientProxyFactory } from '@nestjs/microservices';
+import { authGrpcClientOptions } from './options/grpc-client.options';
 
 @Module({
-  imports: [HttpModule, ConfigModule],
+  imports: [ConfigModule],
   controllers: [AuthProxyController],
+  providers: [
+    {
+      provide: 'AUTH_GRPC_CLIENT',
+      useFactory: () => ClientProxyFactory.create(authGrpcClientOptions),
+    },
+  ],
 })
 export class AuthProxyModule {}
