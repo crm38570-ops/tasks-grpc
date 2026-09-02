754511e104623311b7966954625d408c91870887
crm38570-ops <crm.38570@gmail.com>
2026-09-02 13:08:44 +0300

    chore(tasks-service): swap http env contract for grpc port

--- tasks-service/src/config.chema.ts
+++ tasks-service/src/config.chema.ts
@@ -2,14 +2,10 @@ import Joi from '@hapi/joi';
 
 export const configValidationSchema = Joi.object({
   STAGE: Joi.string().required(),
+  GRPC_PORT: Joi.number().required(),
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().default(5432).required(),
   DB_USERNAME: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
   DB_DATABASE: Joi.string().required(),
-
-  JWT_SECRET: Joi.string().required(),
-  JWT_EXPIRES_IN: Joi.string().required(),
-
-  FILES_GRPC_URL: Joi.string().required(),
 });
