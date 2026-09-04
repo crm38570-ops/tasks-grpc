988743e623064ce97f23228aaf3cdace2f6d52c9
crm38570-ops <crm.38570@gmail.com>
2026-09-04 13:47:36 +0300

    chore(files): use shared data-source options builder

--- files-service/src/database/data-source.ts
+++ files-service/src/database/data-source.ts
@@ -1,17 +1,17 @@
 import 'reflect-metadata';
 import * as dotenv from 'dotenv';
-import { createDataSource } from '@mcs/shared';
 import { DataSource } from 'typeorm';
+import { createDataSourceOptions } from '@mcs/shared';
 import { FileEntity } from '../files/file.entity';
 
 dotenv.config({ path: `.env.stage.${process.env.STAGE || 'dev'}` });
 
-const dataSource: DataSource = createDataSource({
-  database: 'files-service',
-  migrationsTableName: 'files_migrations',
-  migrationsGlob: `${__dirname}/migrations/*{.ts,.js}`,
-  entities: [FileEntity],
-  logging: true,
-});
-
-export default dataSource;
+export default new DataSource(
+  createDataSourceOptions({
+    database: 'files-service',
+    migrationsTableName: 'files_migrations',
+    migrationsGlob: `${__dirname}/migrations/*{.ts,.js}`,
+    entities: [FileEntity],
+    logging: true,
+  }),
+);
